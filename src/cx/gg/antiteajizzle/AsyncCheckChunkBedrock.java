ba6484ec02ca46c8529e9c79bb2c9b9f3775b433
TeaJizzle <tom@tomjervis.co.uk>
2014-06-20 00:50:20 +0100

    made it work

--- src/cx/gg/antiteajizzle/AsyncCheckChunkBedrock.java
+++ src/cx/gg/antiteajizzle/AsyncCheckChunkBedrock.java
@@ -1,8 +1,11 @@
 package cx.gg.antiteajizzle;
 
 import cx.gg.antiteajizzle.AntiTeaJizzle;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChunkSnapshot;
-import org.bukkit.block.Biome;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 /**
@@ -10,9 +13,11 @@ import org.bukkit.scheduler.BukkitRunnable;
  */
 public class AsyncCheckChunkBedrock extends BukkitRunnable {
     ChunkSnapshot chunk;
+    JavaPlugin plugin;
 
-    AsyncCheckChunkBedrock(ChunkSnapshot chunkSnapshot) {
+    AsyncCheckChunkBedrock(ChunkSnapshot chunkSnapshot, JavaPlugin plugin) {
         this.chunk = chunkSnapshot;
+        this.plugin = plugin;
     }
 
     @Override
@@ -20,13 +25,15 @@ public class AsyncCheckChunkBedrock extends BukkitRunnable {
         for(int x = 0; x < 16; x++) {
             for(int z = 0; z < 16; z++) {
                 //check for holes in bedrock
-                if (chunk.getBlockTypeId(x, 1, z)==0) {
-                    AntiTeaJizzle.fixChunk(chunk);
+                if (chunk.getBlockTypeId(x, 1, z)!=7) {
+                	Bukkit.getServer().getScheduler().runTask((Plugin)plugin, new FixChunkTask(chunk));
+                	return;
                 }
                 //check for bedrock above y=1
                 for(int y = 2; y < 7; y++) {
-                    if (chunk.getBlockTypeId(x, y, z)==4) {
-                        AntiTeaJizzle.fixChunk(chunk);
+                    if (chunk.getBlockTypeId(x, y, z)==7) {
+                    	Bukkit.getServer().getScheduler().runTask((Plugin)plugin, new FixChunkTask(chunk));
+                    	return;
                     }
                 }
             }
